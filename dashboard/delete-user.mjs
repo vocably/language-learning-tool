36b746de10030d8483956dc311a49ea38db2702f
Dima Snisarenko <snisarenkodima@gmail.com>
2025-04-21 19:11:40 +0200

    chore: delete user from brevo

--- dashboard/delete-user.mjs
+++ dashboard/delete-user.mjs
@@ -5,6 +5,28 @@ import 'zx/globals';
 
 config();
 
+async function deleteBrevoUser(email) {
+  const encodedEmail = encodeURIComponent(email);
+
+  const response = await fetch(
+    `https://api.brevo.com/v3/contacts/${encodedEmail}`,
+    {
+      method: 'DELETE',
+      headers: {
+        'api-key': process.env.BREVO_KEY,
+        accept: 'application/json',
+      },
+    }
+  );
+
+  if (response.ok) {
+    console.log(`User ${email} deleted successfully from Brevo.`);
+  } else {
+    const errorData = await response.json();
+    console.error(`Failed to delete user from Brevo ${email}:`, errorData);
+  }
+}
+
 const execute = promisify(exec);
 
 const userEmail = process.argv[2];
@@ -14,6 +36,8 @@ const listUsers = `aws cognito-idp list-users --user-pool-id ${process.env.USER_
 const listUsersResult = JSON.parse((await execute(listUsers)).stdout);
 
 console.log('User to be deleted', inspect(listUsersResult, { depth: null }));
+
+await deleteBrevoUser(userEmail);
 
 const sub = listUsersResult.Users[0].Attributes.find(
   (attr) => attr.Name === 'sub'
