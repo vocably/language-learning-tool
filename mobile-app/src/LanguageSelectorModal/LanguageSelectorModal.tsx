df3ee8a32e3e519a72e669e7a303fed42c6deb30
Dima Snisarenko <snisarenkodima@gmail.com>
2025-05-01 09:46:26 +0200

    fix(moa): language selector insets

--- mobile-app/src/LanguageSelectorModal/LanguageSelectorModal.tsx
+++ mobile-app/src/LanguageSelectorModal/LanguageSelectorModal.tsx
@@ -1,13 +1,26 @@
 import { NavigationProp, Route } from '@react-navigation/native';
 import { FC, useCallback, useState } from 'react';
 import { ListRenderItem, Pressable, SectionList, View } from 'react-native';
-import { Appbar, Divider, Text, useTheme } from 'react-native-paper';
+import {
+  Divider,
+  IconButton,
+  Surface,
+  Text,
+  useTheme,
+} from 'react-native-paper';
+import { EdgeInsets, useSafeAreaInsets } from 'react-native-safe-area-context';
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 import { SearchInput } from '../SearchInput';
+import { mainPadding } from '../styles';
+import { ScreenLayout } from '../ui/ScreenLayout';
 import { createLanguageList, LanguageListItem } from './createLanguageList';
 
+type CreateItemProps = {
+  insets: EdgeInsets;
+  onPress: (language: string) => void;
+};
 const createItem =
-  (onPress: (language: string) => void): ListRenderItem<LanguageListItem> =>
+  ({ insets, onPress }: CreateItemProps): ListRenderItem<LanguageListItem> =>
   ({ item }) => {
     const theme = useTheme();
     return (
@@ -18,18 +31,24 @@ const createItem =
               ? theme.colors.inversePrimary
               : theme.colors.background,
           },
-          { padding: 16 },
+          {
+            paddingLeft: insets.left + mainPadding,
+            paddingRight: insets.right + mainPadding,
+            paddingVertical: 16,
+            flexDirection: 'row',
+            alignItems: 'center',
+          },
         ]}
         onPress={() => onPress(item.key)}
       >
-        <Text style={{ fontSize: 16 }}>
-          {item.selected && (
-            <>
-              <Icon name="check" />{' '}
-            </>
-          )}
-          {item.label}
-        </Text>
+        <Text style={{ fontSize: 16, flexGrow: 1 }}>{item.label}</Text>
+        {item.selected && (
+          <Icon
+            name="check"
+            size={24}
+            style={{ color: theme.colors.onBackground }}
+          />
+        )}
       </Pressable>
     );
   };
@@ -61,6 +80,8 @@ export const LanguageSelectorModal: LanguageSelectorModal = ({
   } = route.params;
   const theme = useTheme();
 
+  const insets = useSafeAreaInsets();
+
   const [searchText, setSearchText] = useState('');
 
   const onPress = useCallback(
@@ -72,47 +93,69 @@ export const LanguageSelectorModal: LanguageSelectorModal = ({
   );
 
   return (
-    <View
-      style={{
-        height: '100%',
-        backgroundColor: theme.colors.background,
-      }}
-    >
-      <Appbar.Header statusBarHeight={0}>
-        <Appbar.Content title={title} />
-        <Appbar.Action icon="close" onPress={() => navigation.goBack()} />
-      </Appbar.Header>
-      <View style={{ padding: 8 }}>
-        <SearchInput
-          value={searchText}
-          placeholder="Search"
-          onChange={(value) => {
-            setSearchText(value);
+    <ScreenLayout
+      header={
+        <Surface
+          style={{
+            paddingLeft: insets.left + mainPadding,
+            paddingTop: 8,
+            paddingRight: insets.right + mainPadding,
+            paddingBottom: mainPadding,
           }}
-          onSubmit={() => {}}
-        />
-      </View>
-      <SectionList
-        sections={createLanguageList({
-          selected,
-          preferred,
-          preferredTitle,
-          searchText,
-        })}
-        renderItem={createItem(onPress)}
-        renderSectionHeader={({ section: { title } }) => (
-          <Text
+        >
+          <View
             style={{
-              padding: 16,
-              fontWeight: 'bold',
-              backgroundColor: theme.colors.background,
+              flexDirection: 'row',
+              alignItems: 'center',
+              marginBottom: 8,
             }}
           >
-            {title}
-          </Text>
-        )}
-        ItemSeparatorComponent={Divider}
-      />
-    </View>
+            <Text style={{ flexGrow: 1, fontSize: 18 }}>{title}</Text>
+            <IconButton
+              icon={'close'}
+              onPress={() => navigation.goBack()}
+              style={{ backgroundColor: 'transparent', marginRight: -8 }}
+            />
+          </View>
+          <SearchInput
+            value={searchText}
+            placeholder="Search"
+            onChange={(value) => {
+              setSearchText(value);
+            }}
+            onSubmit={() => {}}
+          />
+        </Surface>
+      }
+      content={
+        <SectionList
+          contentContainerStyle={{ paddingBottom: insets.bottom + mainPadding }}
+          sections={createLanguageList({
+            selected,
+            preferred,
+            preferredTitle,
+            searchText,
+          })}
+          renderItem={createItem({
+            insets,
+            onPress,
+          })}
+          renderSectionHeader={({ section: { title } }) => (
+            <Text
+              style={{
+                paddingLeft: insets.left + mainPadding,
+                paddingRight: insets.right + mainPadding,
+                paddingVertical: 16,
+                fontWeight: 'bold',
+                backgroundColor: theme.colors.background,
+              }}
+            >
+              {title}
+            </Text>
+          )}
+          ItemSeparatorComponent={Divider}
+        />
+      }
+    />
   );
 };
