4015f12c483e4bcc1e25dbffa6ab5fb3c42e01a8
Dima Snisarenko <snisarenkodima@gmail.com>
2025-05-05 13:13:25 +0200

    chore: diable chatgpt test

--- packages/analyze/src/explainSentence.spec.ts
+++ packages/analyze/src/explainSentence.spec.ts
@@ -5,7 +5,7 @@ import { configureTestAnalyzer } from './test/configureTestAnalyzer';
 
 configureTestAnalyzer();
 
-describe('explainSentence', () => {
+xdescribe('explainSentence', () => {
   if (process.env.TEST_SKIP_SPEC === 'true') {
     it('skip spec testing', () => {});
     return;
