101f2bdde7d41da337c2ba6ac502b29afeb7f788
Dima Snisarenko <snisarenkodima@gmail.com>
2025-06-03 11:24:48 +0200

    paid

--- mobile-app/src/useMinutesBeforeNextTranslation.tsx
+++ mobile-app/src/useMinutesBeforeNextTranslation.tsx
@@ -1,9 +1,25 @@
+import { get } from 'lodash-es';
 import { useContext, useEffect, useState } from 'react';
+import { AuthContext, AuthStatus } from './auth/AuthContext';
 import { UserMetadataContext } from './UserMetadataContainer';
 
 const getMinutesBeforeNextTranslation = (
-  lastTranslationTime: number
+  lastTranslationTime: number,
+  authStatus: AuthStatus
 ): number => {
+  if (
+    authStatus.status === 'logged-in' &&
+    (
+      get(
+        authStatus.session,
+        'tokens.accessToken.payload.cognito:groups',
+        []
+      ) as string[]
+    ).includes('paid')
+  ) {
+    return 0;
+  }
+
   const minutesAfterLastTranslation = Math.round(
     (new Date().getTime() - lastTranslationTime) / 60_000
   );
@@ -13,25 +29,29 @@ const getMinutesBeforeNextTranslation = (
 
 export const useMinutesBeforeNextTranslation = () => {
   const { userMetadata } = useContext(UserMetadataContext);
+  const authStatus = useContext(AuthContext);
 
   const [minutesBeforeNextTranslations, setMinutesBeforeNextTranslation] =
     useState<number>(
       getMinutesBeforeNextTranslation(
-        userMetadata.usageStats.lastLookupTimestamp
+        userMetadata.usageStats.lastLookupTimestamp,
+        authStatus
       )
     );
 
   useEffect(() => {
     setMinutesBeforeNextTranslation(
       getMinutesBeforeNextTranslation(
-        userMetadata.usageStats.lastLookupTimestamp
+        userMetadata.usageStats.lastLookupTimestamp,
+        authStatus
       )
     );
 
     const intervalId = setInterval(async () => {
       setMinutesBeforeNextTranslation(
         getMinutesBeforeNextTranslation(
-          userMetadata.usageStats.lastLookupTimestamp
+          userMetadata.usageStats.lastLookupTimestamp,
+          authStatus
         )
       );
     }, 60_000);
@@ -39,7 +59,7 @@ export const useMinutesBeforeNextTranslation = () => {
     return () => {
       clearInterval(intervalId);
     };
-  }, [userMetadata.usageStats.lastLookupTimestamp]);
+  }, [userMetadata.usageStats.lastLookupTimestamp, authStatus]);
 
   return minutesBeforeNextTranslations;
 };
