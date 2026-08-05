d85210090177994c3104067253b03c30dc2cdad2
Dima Snisarenko <snisarenkodima@gmail.com>
2026-08-05 15:03:28 +0200

    chore: sync

--- scripts/de-en.sh
+++ scripts/de-en.sh
@@ -2,8 +2,10 @@
 
 cd ../../vocably-reverse-translations && npm run download-prod
 cd -
-sync de
+cd ../sync-server
+./sync.mts de
 ./build-seo-search-data.mts
+cd -
 cd ../
 git add .
 git commit -m "feat(www-seo): regenerate seo pages"
