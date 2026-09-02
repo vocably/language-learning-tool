c2a010d93a40da25323706c3ac8a6e024c4454af
Dima Snisarenko <snisarenkodima@gmail.com>
2026-09-02 09:38:14 +0200

    feat(extension): youtube subtitles hightlight hover

--- packages/extension-content-script/src/styles.ts
+++ packages/extension-content-script/src/styles.ts
@@ -1,21 +1,27 @@
 export const youtubeHighlightDuration = 50;
 
+const primary = `#0050ff`;
+const darkPrimary = `#28a5ff`;
+
 export const setYouTubeStyles = () => {
   const style = document.createElement('style');
   style.innerHTML = `
   .vocably-word {
     cursor: pointer;
+    display: inline-block;
+    border-radius: 8px;
   }
   .vocably-word:hover {
-    text-decoration: underline;
-    text-decoration-thickness: 1px;
+    background-color: ${primary};
+    box-shadow: -2px 0 0 ${primary}, 2px 0 0 ${primary};
+    color: white;
   }
   .vocably-selectable-captions .ytp-caption-segment {
     transition: text-shadow ${youtubeHighlightDuration}ms ease-in-out;
   }
   .vocably-selectable-captions-highlighted .ytp-caption-segment {
-    text-shadow: 0 0 5px #28a5ff, 0 0 10px #28a5ff, 0 0 20px #28a5ff,
-      0 0 40px #28a5ff !important;
+    text-shadow: 0 0 5px ${darkPrimary}, 0 0 10px ${darkPrimary}, 0 0 20px ${darkPrimary},
+      0 0 40px ${darkPrimary} !important;
   }
 `;
   document.head.appendChild(style);
